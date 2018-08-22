86c362e1192bcf6a251f35b6ac25fa6fa368a30c
bob-perez-utah <bperez@gw.novell.com>
2018-08-21 21:30:26 -0600

    initial commit

---
+++ function.js
@@ -0,0 +1,16 @@
+function isEven(number) {
+
+        if (number % 2 === 0) {
+            console.log("true");
+            return number % 2 === 0;
+
+        } else
+                console.log("false");
+                return number % 2 !== 0;
+
+}
+
+isEven(1);
+isEven(2);
+isEven(997);
+isEven(2002);
