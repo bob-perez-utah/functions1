aaf8854bd6d4545b65e495f549ba4cee741af327
bob-perez-utah <bperez@gw.novell.com>
2018-08-28 20:03:32 -0600

    final commit

--- function.js
+++ function.js
@@ -10,6 +10,27 @@ function isEven(number) {
 
 }
 
+function factorial(num) {
+    //defaine a result variable
+    var result = 1;
+
+   for(var i = 2; i <= num; i++){
+       result *= i;
+   }
+   return result;
+}
+
+function kebabToSnake(str) {
+    // replace all '-' with "_"'s
+    // return str
+
+    
+    var newStr = str.replace(/-/g, "_");
+    
+    return newStr;
+}
+
+
 isEven(1);
 isEven(2);
 isEven(997);
